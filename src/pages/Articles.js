c5eb61a0ca5d964b5fe8bc249ec57b1762ff32ee
JunHwan Kim <nefertiri972@gmail.com>
2022-06-17 00:04:13 +0900

    Refactor NavLink code
    - ArticleItem 컴포넌트 추가하여 중복 코드 제거

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -11,6 +11,10 @@ const Articles = () => {
     <div>
       <Outlet />
       <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+        {/* 
         <li>
           <NavLink
             to="/articles/1"
@@ -35,8 +39,29 @@ const Articles = () => {
             게시글 3
           </NavLink>
         </li>
+         */}
       </ul>
     </div>
+  );
+};
+
+const ArticleItem = ({ id }) => {
+  // 구조 분해 할당
+
+  const activeStyle = {
+    color: 'green',
+    fontSize: 21,
+  };
+
+  return (
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)} // 구조 분해 할당
+      >
+        {`게시글 ${id}`}
+      </NavLink>
+    </li>
   );
 };
 
