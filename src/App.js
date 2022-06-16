018d53cf37e4b2803fde1fa03f81d7531f6977e2
JunHwan Kim <nefertiri972@gmail.com>
2022-06-16 20:14:02 +0900

    Add Common Layout Component
    - 중첩된 라우트와 Outlet을 이용하여 공통 레이아웃 구현
    - 페이지 상단에 헤더를 보여주는 상황 구현
    - Route 컴포넌트의 index는 path="/"와 동일한 의미를 가짐

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
