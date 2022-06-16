5efc9ec0243a910600c845d7aa87fae27765c528
JunHwan Kim <nefertiri972@gmail.com>
2022-06-16 23:53:14 +0900

    Add useNavigate
    - useNavigate는 Link 컴포넌트를 사용하지 않고 다른 페이지로 이동할 때 사용할 수 있는 Hook
    - navigate 함수 호출시 인자로 replace 값이 true시 goArticles 호출 시점에 떠있는 페이지를 기록에 남기지 않음

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1); // 뒤로 한 번 가기
+  };
+
+  const goArticles = () => {
+    navigate('/articles', { replace: true }); // replace true시 goArticles 호출 시점에 떠있는 페이지를 기록에 남기지 않음
+  };
+
   return (
     <div>
       <header style={{ background: 'lightgray', padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록</button>
       </header>
       <main>
         <Outlet />
