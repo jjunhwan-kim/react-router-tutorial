948209c2a40e625269c31f9d86282373df5882d4
JunHwan Kim <nefertiri972@gmail.com>
2022-06-16 17:44:43 +0900

    Add nested Route
    - Articles 컴포넌트에서 Article 컴포넌트를 중첩하여 출력
    - App 컴포넌트에서 Route의 자식 요소로 Route를 중첩
    - Articles 컴포넌트에서 Outlet 컴포넌트를 사용하여 Route의 자식 요소인 Article 컴포넌트도 같이 표시되도록 함

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,13 +13,14 @@ const Home = () => {
         <li>
           <Link to="/profiles/velopert">velopert의 프로필</Link>
         </li>
-
         <li>
           <Link to="/profiles/gildong">gildong의 프로필</Link>
         </li>
-
         <li>
           <Link to="/profiles/void">존재하지 않는 프로필</Link>
+        </li>
+        <li>
+          <Link to="/articles">게시글 목록</Link>
         </li>
       </ul>
     </div>
